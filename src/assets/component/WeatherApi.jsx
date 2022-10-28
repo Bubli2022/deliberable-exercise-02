3d2ad4ef07a57e53589ba98756982f0858220f51
Ariel <bublicius2010@gmail.com>
2022-10-28 11:34:18 -0300

    first commit

---
+++ src/assets/component/WeatherApi.jsx
@@ -0,0 +1,64 @@
+import React from "react"
+import axios from "axios"
+import { useState } from "react"
+import { useEffect } from "react"
+
+const WeatherApi = () => {
+   const [weather, setWeather] = useState({})
+
+   useEffect(() => {
+      navigator.geolocation.getCurrentPosition(success)
+
+      function success(pos) {
+         const crd = pos.coords
+
+         console.log("Your current position is:")
+         console.log(`Latitude : ${crd.latitude}`)
+         console.log(`Longitude: ${crd.longitude}`)
+         console.log(`More or less ${crd.accuracy} meters.`)
+         axios
+            .get(
+               `https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=cf74ae059a14d1682d4513a52d4319db&units=metric`
+            )
+            .then((res) => setWeather(res.data))
+      }
+   }, [])
+
+   console.log(weather)
+
+   const [isCelcius, setIsCelcius] = useState(true)
+
+   const changeUnit = () => {
+      setDegrees(!isCelcius)
+   }
+
+   return (
+      <div>
+         <div className="card">
+            {" "}
+            <h1 className="weather--gif">WEATHER APP</h1>{" "}
+            <img
+               src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}.png`}
+               alt=""
+            />
+            <h2>
+               {weather.name}, {weather.sys?.country}
+            </h2>
+            <h3>
+               {isCelcius
+                  ? weather.main?.temp
+                  : weather.main?.temp * (9 / 5) + 32}{" "}
+               {isCelcius ? "°C" : "°F"} {weather.weather?.[0].description}
+            </h3>
+            <h3> Wind Speed {weather.wind?.speed}m/s</h3>
+            <h3>Clouds: {weather.clouds?.all}%</h3>
+            <h3>Pressure: {weather.main?.pressure} hPa</h3>
+            <button onClick={() => setIsCelcius(!isCelcius)}>
+               Degrees °C / °F {isCelcius}
+            </button>
+         </div>
+      </div>
+   )
+}
+
+export default WeatherApi
